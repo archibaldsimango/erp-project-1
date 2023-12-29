9e776f6a0a6aeeb0e23696494762a625364162c6
archibaldsimango <archibaldsimango@gmail.com>
2023-12-29 23:10:23 +0200

    feature: added helper functions

--- apps/erp-dashboard/src/app/components/navigation/Navbar.tsx
+++ apps/erp-dashboard/src/app/components/navigation/Navbar.tsx
@@ -1,9 +1,11 @@
 import { data } from "@utils/data"
 import { Link } from "react-router-dom"
+import useCurrentPath from "@hooks/useCurrentPath"
 
 type Props = {}
 
 function Navbar({ }: Props) {
+    const currentPath = useCurrentPath()
     return (
         <nav className='fle flex-row items-center py-4 '>
             <div className="mx-w-7xl px-4 w-full mx-auto flex space-x-4 flex-row items-center">
@@ -11,7 +13,7 @@ function Navbar({ }: Props) {
                 <div className="flex flex-row space-x-4 flex-1">
                     {
                         data.nav_options.map(item => (
-                            <Link className="text-sm font-medium" to={item.location} key={item._id}>{item.name}</Link>
+                            <Link className={`${currentPath === item.location ? "text-primary-original font-extrabold " : "text-slate-700 "} text-sm font-medium`} to={item.location} key={item._id}>{item.name}</Link>
                         ))
                     }
                 </div>
