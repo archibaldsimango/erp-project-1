d5e63c4e4cf38a19aab8a223487414f8ce332f3d
archibaldsimango <archibaldsimango@gmail.com>
2023-12-29 23:01:36 +0200

    feature: added color scheme to tailwinf

--- apps/erp-dashboard/tailwind.config.js
+++ apps/erp-dashboard/tailwind.config.js
@@ -8,7 +8,24 @@ module.exports = {
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          original: "#4c6085",
+          dark: "#35435d",
+          light: "#8290aa",
+          superdark: "#1e2635",
+          superlight: "#b7bfce",
+        },
+        secondary: {
+          original: "#32322c",
+          dark: "#23231f",
+          light: "#70706b",
+          superdark: "#141412",
+          superlight: "#adadab",
+        },
+      },
+    },
   },
   plugins: [],
 };
